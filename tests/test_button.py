e6966a58c9b490443da7797c9045714e53dea297
andrey.yesipionak <esipenokand@gmail.com>
2024-11-18 21:36:29 +0300

    Добавление кода с безголовым запуском и файл с запуском автотеста.

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
+from selenium.webdriver.chrome.options import Options # это для безголового режима
 
 @pytest.fixture()
 def browser():
-    crome_browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless') # так мы запускаем безголовый режим. Нужен для запуска автотестов в гитхабе
+    crome_browser = webdriver.Chrome(options=options)
     return crome_browser
 
 def test_button_exist(browser):
