160e5d4b7d3a81d64278687bd1cc25508149e91a
Andrey_qa <esipenokand@gmail.com>
2024-12-23 19:52:36 +0300

    Добавление нового файла

--- tests/test_button.py
+++ tests/test_button.py
@@ -2,6 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
 from selenium.webdriver.chrome.options import Options  # это для безголового режим
+import allure
+
+#что бы установить, нужно прописать pip3 install allure-pytest
+#что бы запустить allure, нужно прописать pytest -s -v --alluredir results
+#что бы собрать отчет, нужно прописать allure serve results
+#что бы собрать папку с отчетом, пишем allure generate results
 
 
 @pytest.fixture()
@@ -12,13 +18,20 @@ def browser():
     return crome_browser
 
 
+@allure.feature('first_test')#Так мы указываем, в какую группу попадет данный тест. Можно писать название стори или фичи.
 # первый тест
+@allure.story('displayed')# Это уже идет как подкатегория. Тут можно писать элементы стори или фичи
 def test_button_exist_1(browser):
     browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
 
 
+
+@allure.feature('second_test')
 # второй тест
+@allure.story('displayed_2')
 def test_button_exist_2(browser):
-    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
-    assert browser.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
+    with allure.step('Open the page'):#Так мы можем указывать шаги, которые выполняются
+        browser.get('https://www.qa-practice.com/elements/button/like_a_button')
+    with allure.step('Click'):
+        assert browser.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
